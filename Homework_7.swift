7bd645d716407c4fdf32163d934f496a5151815e
OlexSQ <41807058+OlexSQ@users.noreply.github.com>
2018-08-12 20:42:22 +0300

    Create Homework_7.swift

---
+++ Homework_7.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let num0 = "153"
+let num1 = "47"
+let num2 = "56"
+let num3 = "9820"
+let num4 = "7"
+
+let summ = Int(num0)! + Int(num1)! + Int(num2)! + Int(num3)! + Int(num4)!
+
+let number : Int? = nil
+let symbol : String? = nil
+let dicimal : Double? = nil
+
+let intNumber : Int? = 28
+let floatNumber : Float? = 12.347
+let doubleNumber : Double? = 76.8974561
+let string : String? = "String"
+let boolean : Bool? = false
+
+let name = "Oleg"
+for i in name {
+    print(i)
+}
+
+let ages = [25, 1, 24]
+for i in ages {
+    print(i)
+}
